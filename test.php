5bc2287715a2f420ab89186c142c26cea0a3cc82
Snegativr <93019177+Snegativr@users.noreply.github.com>
2024-03-26 10:01:13 +0200

    Add files via upload

--- test.php
+++ test.php
@@ -26,29 +26,25 @@ function divide($a, $b)
 
 function customSort($arr)
 {
-    sort($arr); // Неправильное использование sort()
+    sort($arr);
 
     return $arr;
 }
 
-// Тестирование функции проверки валидации email адреса
 assert(validateEmail("example@example.com") === true);
 assert(validateEmail("invalid_email") === false);
 assert(validateEmail("another_example@example.com") === true);
 
-// Тестирование функции вычисления факториала
 assert(factorial(0) === 1);
 assert(factorial(1) === 1);
 assert(factorial(5) === 120);
 
-// Тестирование функции деления
 assert(divide(10, 2) === 5);
 assert(divide(100, 0) === "На ноль делить нельзя");
 assert(divide(8, 2) === 4);
 
-// Тестирование функции сортировки
 assert(customSort([3, 2, 1]) === [1, 2, 3]);
 assert(customSort(['c', 'b', 'a']) === ['a', 'b', 'c']);
-assert(customSort([5, 4, 6]) === [4, 5, 6]); // Неверный ожидаемый результат
+assert(customSort([5, 4, 6]) === [4, 5, 6]);
 
 echo "Все тесты успешно пройдены!";
